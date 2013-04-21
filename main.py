1cb1fbea791946e93d9a23ce15bdf3dd0c631b7b
Michael Mimo Moratti <mimo@mimo.ch>
2013-04-21 14:17:15 +0200

    added login mask

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+#    Copyright (c) 2013 Michael Mimo Moratti.
+#   
+#    Michael Mimo Moratti licenses this file to you under the Apache License, version 2.0
+#    (the "License"); you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at:
+#        http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 import webapp2
+import os
+from google.appengine.ext.webapp import template
+from google.appengine.api import users
 
 class MainHandler(webapp2.RequestHandler):
+	
     def get(self):
-        self.response.write('Hello world!')
-
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+		user = users.get_current_user()
+		if user:
+			template_values = {
+				'title': 'Weather Site'
+			}
+			path = os.path.join(os.path.dirname(__file__),'html/index.html')
+			self.response.out.write(template.render(path, template_values))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+		
+app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
